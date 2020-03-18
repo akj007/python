3a0b00a52ee5d4e5194e0df9e5ae1f80d5b1c9c0
Akshat Jain <35661554+akj007@users.noreply.github.com>
2020-03-18 10:13:32 +0530

    Update bot.py

--- twitter/bot.py
+++ twitter/bot.py
@@ -3,10 +3,10 @@ import time
 import os
 
 
-consumer_key = 'AHDPRwJtdAh12FTdoepiu48xw'
-consumer_secret = 'Z1AbHwu0vHqjZ6B01rsxKgs5xEeAyDRutlpKVVvUDdhwmJ5NC4'
-access_token = '1181276838481391619-AU4XQoPGeYgT7ivPk3Wr5RLbiIlZCy'
-access_secret = 'zFq7BfcIbTCxUMz6YF6uy9iUYpPoRiNYNSImBYMyYXAq7'
+consumer_key = '**********'
+consumer_secret = '**********'
+access_token = '**********'
+access_secret = '**********'
 
 auth = tp.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_secret)
